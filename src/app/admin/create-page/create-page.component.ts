c5f1c444718491c01e53b9eb800ae1c2e419af69
Konstantin Puganov <kostyanp95@yandex.ru>
2020-07-23 02:03:08 +0300

    docs(create-page): added documentation for create-page post

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Post } from '../../shared/interfaces';
 
+/**
+ * Компонет с формой для создания поста
+ */
 @Component({
     selector: 'app-create-page',
     templateUrl: './create-page.component.html',
@@ -9,12 +12,18 @@ import { Post } from '../../shared/interfaces';
 })
 export class CreatePageComponent implements OnInit {
 
+    /**
+     * Форма создания поста
+     */
     createPostForm: FormGroup;
 
     constructor(private fb: FormBuilder) {
     }
 
     ngOnInit(): void {
+        /**
+         * Инициализация формы создания поста
+         */
         this.createPostForm = this.fb.group({
             title: [null, [Validators.required]],
             text: [null, [Validators.required]],
@@ -22,11 +31,15 @@ export class CreatePageComponent implements OnInit {
         });
     }
 
+    /**
+     * Создание поста при клике на кнопку
+     */
     createPost(): void {
         if (this.createPostForm.invalid) {
             return;
         }
 
+        // Создание поста на основе заполненных инпутов
         const post: Post = {
             title: this.createPostForm.value.title,
             text: this.createPostForm.value.text,
