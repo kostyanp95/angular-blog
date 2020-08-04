449cc56b531635fcad1572e75b70a7e55d470421
Konstantin Puganov <kostyanp95@yandex.ru>
2020-08-05 01:48:34 +0300

    feat(project): added PWA and preparing for a test prod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { PostPageComponent } from './post-page/post-page.component';
 import { PostComponent } from './shared/components/post/post.component';
 import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './shared/auth.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 /**
  * Локализация времени
@@ -38,7 +40,8 @@ const INTERCEPTOR_PROVIDER: Provider = {
     imports: [
         BrowserModule,
         AppRoutingModule,
-        SharedModule
+        SharedModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [
         INTERCEPTOR_PROVIDER,
